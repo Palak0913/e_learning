9177aa301dd90fbdcd947a6b58e4d2fc85905f39
Palak <palaktak13@gmail.com>
2020-11-23 16:31:09 +0530

    deploy to heroku

--- e_learning/settings.py
+++ e_learning/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -140,6 +140,6 @@ REST_FRAMEWORK = {
     ]
 }
 
-
+django_heroku.settings(locals())
 
 #CSRF_COOKIE_NAME = "XSRF-TOKEN"
